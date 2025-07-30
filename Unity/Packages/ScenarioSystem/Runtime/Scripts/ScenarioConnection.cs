a5dabc9b0390a4d5d0c7ce0a6ec17145d69b5b23
SoheilPmi <139005666+SoheilPmi@users.noreply.github.com>
2025-07-30 11:24:28 +0330

    Create ScenarioConnection.cs

---
+++ Unity/Packages/ScenarioSystem/Runtime/Scripts/ScenarioConnection.cs
@@ -0,0 +1,9 @@
+namespace ScenarioSystem.Runtime
+{
+    [System.Serializable]
+    public class ScenarioConnection
+    {
+        public ScenarioNodeBase fromNode;
+        public ScenarioNodeBase toNode;
+    }
+}
